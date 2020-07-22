3511ce93da04faebe188c1122eab9add7562e62c
Kim Christensen <ktc@kb.dk>
2020-07-22 10:10:31 +0200

    Add additional java options for enabling support for DES encryption

--- src/main/scripts/doms-pidregistration.sh
+++ src/main/scripts/doms-pidregistration.sh
@@ -2,5 +2,7 @@
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-java -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile="$SCRIPT_DIR/../conf/logback.xml" -Ddk.kb.applicationConfig="$SCRIPT_DIR/../conf/doms-pidregistration.properties" -jar "$SCRIPT_DIR/../lib/doms-pidregistration.jar" "$@"
+JAVA_OPTS="-Djdk.crypto.KeyAgreement.legacyKDF=true"
+
+java "$JAVA_OPTS" -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile="$SCRIPT_DIR/../conf/logback.xml" -Ddk.kb.applicationConfig="$SCRIPT_DIR/../conf/doms-pidregistration.properties" -jar "$SCRIPT_DIR/../lib/doms-pidregistration.jar" "$@"
 
