ed4aea96114ff9baf2722ca6a8e56e9494eb689d
Kim Christensen <ktc@kb.dk>
2020-07-22 09:15:05 +0200

    Fix packaing and add mention of proper prefix namespace for test

--- src/main/scripts/doms-pidregistration.sh
+++ src/main/scripts/doms-pidregistration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-java -Dlogback.configurationFile="$SCRIPT_DIR/../conf/logback.xml" -Ddk.kb.applicationConfig="$SCRIPT_DIR/../conf/doms-pidregistration.properties" -jar doms-pidregistration.jar "$@"
+java -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile="$SCRIPT_DIR/../conf/logback.xml" -Ddk.kb.applicationConfig="$SCRIPT_DIR/../conf/doms-pidregistration.properties" -jar "$SCRIPT_DIR/../lib/doms-pidregistration.jar" "$@"
 
