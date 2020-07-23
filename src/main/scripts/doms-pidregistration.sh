a796969970d8397190a3f37320adf716361cae5f
Kim Christensen <ktc@kb.dk>
2020-07-23 10:45:12 +0200

    Update run script and fix assembly

--- src/main/scripts/doms-pidregistration.sh
+++ src/main/scripts/doms-pidregistration.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pushd  $(dirname $0) > /dev/null
-java -Dlog4j.configuration=file://$HOME/doms-pidregistration.log4j.xml -jar doms-pidregistration.jar "$@"
-popd > /dev/null
+
+SCRIPT_DIR=$(dirname "$(readlink -f -- ${BASH_SOURCE[0]})")
+
+JAVA_OPTS="-Djdk.crypto.KeyAgreement.legacyKDF=true"
+
+java "$JAVA_OPTS" -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile="$SCRIPT_DIR/../conf/logback.xml" -Ddk.kb.applicationConfig="$SCRIPT_DIR/../conf/doms-pidregistration.properties" -jar "$SCRIPT_DIR/../lib/doms-pidregistration.jar" "$@"
+
